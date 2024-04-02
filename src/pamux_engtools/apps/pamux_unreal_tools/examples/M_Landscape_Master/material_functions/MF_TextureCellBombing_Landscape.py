f3a3774c9621532c980218e17b39faa49cb34101
baris <yazici.b@gmail.com>
2024-04-02 02:37:33 -0700

    fixes

--- src/pamux_engtools/apps/pamux_unreal_tools/examples/M_Landscape_Master/material_functions/MF_TextureCellBombing_Landscape.py
+++ src/pamux_engtools/apps/pamux_unreal_tools/examples/M_Landscape_Master/material_functions/MF_TextureCellBombing_Landscape.py
@@ -31,20 +31,21 @@ class MF_TextureCellBombing_Landscape:
             self.cellScale = builder.build_FunctionInput("CellScale", unreal.FunctionInputType.FUNCTION_INPUT_SCALAR, Constant(2.0))
             self.doRotationVariation = builder.build_FunctionInput("DoRotationVariation", unreal.FunctionInputType.FUNCTION_INPUT_STATIC_BOOL, StaticBool(False))
             self.isNormalMap = builder.build_FunctionInput("IsNormalMap", unreal.FunctionInputType.FUNCTION_INPUT_STATIC_BOOL, StaticBool(False))
-            self.texture = builder.build_FunctionInput("Texture", unreal.FunctionInputType.FUNCTION_INPUT_TEXTURE2D, TextureObject())
+
+            self.texture = builder.build_FunctionInput("Texture", unreal.FunctionInputType.FUNCTION_INPUT_TEXTURE2D, TextureObject(unreal.MaterialSamplerType.SAMPLERTYPE_COLOR, None))
             self.texture.use_preview_value_as_default.set(False)
 
-            self.UVs3Vector = AppendVector(self.inputs.UVs.rt, Constant(0.0))
+            self.UVs3Vector = AppendVector(self.UVs.rt, Constant(0.0))
 
-            self.patternScaledUVs3Vector = Multiply(self.UVs3Vector, self.inputs.patternScale.rt)
+            self.patternScaledUVs3Vector = Multiply(self.UVs3Vector, self.patternScale.rt)
             self.patternScaledUVs3VectorRG = ComponentMask(self.patternScaledUVs3Vector, "RG")
 
-            self.cellScaledUVs3Vector = Multiply(self.UVs3Vector, self.inputs.cellScale.rt)
+            self.cellScaledUVs3Vector = Multiply(self.UVs3Vector, self.cellScale.rt)
             self.cellScaledUVs3VectorRG = ComponentMask(self.cellScaledUVs3Vector, "RG")
 
             self.patternScaledInputTexture = TextureSample()
             self.patternScaledInputTexture.UVs.comesFrom(self.patternScaledUVs3VectorRG)
-            self.patternScaledInputTexture.tex.comesFrom(self.inputs.texture.rt)
+            self.patternScaledInputTexture.tex.comesFrom(self.texture.rt)
             self.patternScaledInputTexture.sampler_source.set(unreal.SamplerSourceMode.SSM_WRAP_WORLD_GROUP_SETTINGS)
             self.patternScaledInputTexture.sampler_type.set(unreal.MaterialSamplerType.SAMPLERTYPE_COLOR)
             self.patternScaledInputTexture.rt = NamedRerouteDeclaration(f"rtPatternScaledInputTexture", self.patternScaledInputTexture.RGB)
@@ -64,26 +65,27 @@ class MF_TextureCellBombing_Landscape:
 
 
         def __build_RandomRotate(self):
-            pivotPoint = Constant3Vector()
+            pivotPoint = Constant3Vector(None)
 
-            self.worldPosition = Add(self.patternScaledUVs3Vector, self.offsetVariation)
+            self.worldPosition = Add(self.inputs.patternScaledUVs3Vector, self.offsetVariation)
 
             self.rotateAboutWorldAxis_cheap.call()
             self.rotateAboutWorldAxis_cheap.call_result.rotationAmount.comesFrom(self.rotationVariation)
             self.rotateAboutWorldAxis_cheap.call_result.pivotPoint.comesFrom(pivotPoint)
             self.rotateAboutWorldAxis_cheap.call_result.worldPosition.comesFrom(self.worldPosition)
 
-            usage = NamedRerouteUsage()
-            usage.asset.set_editor_property("Declaration", self.inputs.doRotationVariation.rt)
+            usage = NamedRerouteUsage(None)
+            #usage.asset.set_editor_property("Declaration", self.inputs.doRotationVariation.rt)
 
-            switch = StaticSwitch(Add(self.rotateAboutWorldAxis_cheap.call_result.zAxis, self.worldPosition), self.worldPosition, usage ) # self.inputs.doRotationVariation.rt
+            # self.inputs.doRotationVariation.rt
+            switch = StaticSwitch(Add(self.rotateAboutWorldAxis_cheap.call_result.zAxis, self.worldPosition), self.worldPosition, usage)
 
             self.randomRotateRG = ComponentMask(switch, "RG")
 
-            return self.randomRotateRG.output, self.patternScaledUVs3VectorRG.output
+            #return self.randomRotateRG.output, self.inputs.patternScaledUVs3VectorRG.output
         
         def __build_NormalRotation(self):
-            pivotPoint = Constant3Vector()
+            pivotPoint = Constant3Vector(None)
 
             self.rotatedInputTexture = TextureSample()
             self.rotatedInputTexture.UVs.comesFrom(self.randomRotateRG)
@@ -105,20 +107,25 @@ class MF_TextureCellBombing_Landscape:
             switch1 = StaticSwitch(self.normalRotation, self.rotatedInputTexture.rt, self.inputs.isNormalMap.rt)
             switch2 = StaticSwitch(switch1, self.rotatedInputTexture.rt, self.inputs.doRotationVariation.rt)
 
-            self.lerp = LinearInterpolate(self.patternScaledInputTexture.rt, switch2, self.textureSample.a)
+            self.lerp = LinearInterpolate(self.inputs.patternScaledInputTexture.rt, switch2, self.textureSample.a.rt)
 
         def __build_PostInput(self):
             self.textureSample = TextureSample()
-            self.textureSample.UVs.comesFrom(self.cellScaledUVs3VectorRG)
+            self.textureSample.UVs.comesFrom(self.inputs.cellScaledUVs3VectorRG)
             self.textureSample.sampler_source.set(unreal.SamplerSourceMode.SSM_WRAP_WORLD_GROUP_SETTINGS)
             self.textureSample.sampler_type.set(unreal.MaterialSamplerType.SAMPLERTYPE_LINEAR_COLOR)
             self.textureSample.texture.set(unreal.load_asset("/Script/Engine.Texture2D'/Game/Materials/Functions/TextureCellBombing/T_Voronoi_Perturbed_4k.T_Voronoi_Perturbed_4k'"))
+            
+            self.textureSample.RGB.rt = NamedRerouteDeclaration(f"rtTextureSample.RGB", self.textureSample.RGB)
+            self.textureSample.r.rt = NamedRerouteDeclaration(f"rtTextureSample.R", self.textureSample.r)
+            self.textureSample.a.rt = NamedRerouteDeclaration(f"rtTextureSample.A", self.textureSample.a)
 
-            self.offsetVariation = Multiply(self.inputs.randomOffsetVariation.rt, self.textureSample.RGB)
-            self.rotationVariation = Multiply(self.textureSample.r, self.inputs.randomRotationVariation.rt)
+
+            self.offsetVariation = Multiply(self.inputs.randomOffsetVariation.rt, self.textureSample.RGB.rt)
+            self.rotationVariation = Multiply(self.textureSample.r.rt, self.inputs.randomRotationVariation.rt)
             self.negativeRotationVariation = Multiply(self.rotationVariation, -1.0)
 
-            return self.patternScaledUVs3Vector.output, self.textureSample.a, self.offsetVariation.output, self.rotationVariation.output
+            # return self.inputs.patternScaledUVs3Vector.output, self.textureSample.a.rt, self.offsetVariation.output, self.rotationVariation.output
 
         def build_process_nodes(self):
             self.__build_PostInput()
--- src/pamux_engtools/apps/pamux_unreal_tools/utils/build_stack.py
+++ src/pamux_engtools/apps/pamux_unreal_tools/utils/build_stack.py
@@ -3,29 +3,21 @@ class Stack:
         self.items = []
 
     def push(self, value):
-        print(f"prepush {len(self.items)}")
         result = self.items.append(value)
-        print(f"postpush {len(self.items)}")
         return result
     
     def pop(self):
-        print(f"prepop {len(self.items)}")
         if len(self.items) == 0:
-            print(f"postpop {len(self.items)}")
             return None
         result = self.items.pop()
-        print(f"postpop {len(self.items)}")
         return result
     
     def top(self):
-        print(f"pretop {len(self.items)}")
         if len(self.items) == 0:
             return None
         return self.items[len(self.items)-1]
 
-print("BuildStack")
 BuildStack = Stack()
-
 
 class NodePos:
     DeltaX = 400
